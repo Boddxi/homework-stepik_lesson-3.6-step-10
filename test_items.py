d842d9f357348a0b1a874974e60d8906b37453df
unknown <juleta-petrova@yandex.ru>
2024-09-06 13:20:21 +0300

    the last option

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ def test_page_contains_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     cart_button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert cart_button.is_displayed()
-    time.sleep(10)
+    time.sleep(30)
